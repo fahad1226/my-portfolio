9716bff7417389785aed374ed1e483172cb0da10
fahad1226 <fahadbinmnr@gmail.com>
2025-02-21 00:02:58 +0600

    removed the sentry data

---
+++ next.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
+        formats: ['image/avif', 'image/webp'],
+    },
+    env: {
+        // API_BASE_URL: process.env.API_BASE_URL,
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+
+
+
+};
+
+module.exports = nextConfig;
