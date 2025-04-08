258ab10a1540e0fdd0666baa3c1e94974e219e6a
fahad1226 <fahadbinmnr@gmail.com>
2025-04-08 17:30:47 +0600

    Update package.json scripts and integrate Google Tag Manager for enhanced tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Fira_Sans } from "next/font/google";
 
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
@@ -23,9 +24,29 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
+                {/* Google Tag Manager */}
+                <Script
+                    id="google-tag-manager"
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-TVTG8GR3');`,
+                    }}
+                />
                 <link rel="icon" href="/jsm-logo.png" sizes="any" />
             </head>
             <body className={firaSans.className}>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-TVTG8GR3"
+                        height="0"
+                        width="0"
+                        style={{ display: "none", visibility: "hidden" }}
+                    ></iframe>
+                </noscript>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="dark"
